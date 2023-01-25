56a3f5f792583fa2184f9c8fe7eba175b91f0ae0
daroG <darek.kce@gmail.com>
2023-01-25 21:18:25 +0100

    feat(main): change app entry point to main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
+
+import uvicorn
+
+
 def main() -> None:
-    """Main function printing hello world"""
-    print('Hello world')
+    """Main function running the app"""
+    uvicorn.run('rest_image_tracker.api:app', host='0.0.0.0', port=int(os.getenv('PORT', '80')))
 
 
 if __name__ == '__main__':
